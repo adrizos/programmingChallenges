d64e2efc07310dfdb6d72dd5b43f4444ddef3106
Alex Drizos <adrizosmpb@Alexanders-MacBook-Pro.local>
2019-01-13 23:26:14 -0500

    more work

--- UniqueStrings.js
+++ UniqueStrings.js
@@ -1,5 +1,18 @@
 var string = "Hbdsad dfk";
 
+//check if there are any matching touching pairs
 function isUnique (string){
-  
+  var count = 0;
+  newString = string.split('');
+  for (var i = 0; i < newString.length-1; i++) {
+    for (var x = 0; x < newString.lenght; x++){
+      if (newString[i] === newString[x]){
+        count+= 1;
+      }
+    }
+  }
+  return count;
 }
+
+
+//are there any unique characters (using additional memory)
